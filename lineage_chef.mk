0b6ff25a5a601d032293e1871c657c30b09604c3
Rohan Hasabe <rohanhasabe8@gmail.com>
2020-06-14 06:22:21 +0530

    chef: Revert back to 8.1 Build FP
    Change-Id: I138704747cafce2ad8845a41339f9f07b627282f

--- lineage_chef.mk
+++ lineage_chef.mk
@@ -70,7 +70,7 @@ PRODUCT_RELEASE_NAME := chef
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=chef
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC=" 10 QPTS30.61-18-3 9514c release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="8.1.0/OPT28.81-45/2544:user/release-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := motorola/chef_retail/chef_sprout:10/QPTS30.61-18-3/9514c:user/release-keys
+BUILD_FINGERPRINT := 8.1.0/OPT28.81-45/2544:user/release-keys
