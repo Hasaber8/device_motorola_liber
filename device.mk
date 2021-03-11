934d88a24e9d8710363790093ffabbb4ce2abf5b
Rohan Hasabe <rohanhasabe8@gmail.com>
2021-03-11 18:30:35 +0100

    liber: Kang popup impl from raphael
    Change-Id: I99967dcc878bf0b3aebc5fae4301ba69d32fde0d

--- device.mk
+++ device.mk
@@ -31,6 +31,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/mixer_paths_awinic_snd.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_awinic_snd.xml \
     $(LOCAL_PATH)/audio/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths.xml
 
+# Device-specific settings
+PRODUCT_PACKAGES += \
+    MotoParts
+
 # Init
 PRODUCT_PACKAGES += \
     init.mmi.laser.sh \
